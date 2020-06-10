da2bc693916ac09e1b122e3b378477221dceab7a
ibraim-Ajarmeh <ajarmeh1997@gmail.com>
2020-06-10 22:16:45 +0300

    add [2~ms to the toggle

--- js/app.js
+++ js/app.js
@@ -4,12 +4,12 @@
 $(".slide-container").slick({
 	dots: true,
 	autoplay: true,
-	autoplaySpeed: 35000,
+	autoplaySpeed: 3500,
 	pauseOnDotsHover: false,
 	fade: true,
 });
 
 $('.icon').click(function (e) { 
 	e.preventDefault();
-	$('.main_nav').toggle();
+	$('.main_nav').toggle(500);
 });
